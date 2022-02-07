641aab2e42c03ed75263f30bea234b25d9648ae7
SucdiC <55110783+SucdiC@users.noreply.github.com>
2022-02-07 17:02:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,38 +38,4 @@ planner();
 
 
 
-// function additems(){
-   
-//     var listedItem= prompt("Enter item into To-Do list:");
-//     var newItem={item:listedItem};
-//     toDo.push(newItem);
-    
-// }
-
-
-// var entry=2;
-// for (var i=0; i<entry; i++){
-//     additems();
-// }
-
-// function displayitems(){
-//     //    array=0 as a place holder to loop through bookArray
-//         var array=0;
-        
-//         // entries only used to identify book number
-//         for(entries=1; entries!=7; entries++){
-//         // bookArray[array].whatever targets the book entries' title, author, genre for each index of array. 0 for book1 etc.
-//         var li = document.createElement("li");
-//         li.appendChild(document.createTextNode(toDo[array].item));
-//         document.querySelector("ul").appendChild(li);
-//         array++;
-    
-    
-//         }
-    
-//     }
-// displayitems();
-
-
-
 
